406d0e6573c529966ca6dcc7a790126d2e984002
Existime <hassan.alhawari98@gmail.com>
2021-05-21 23:16:02 +0200

    Superhero visual update 1.0

---
+++ app/dao/SuperheroDAO.scala
@@ -0,0 +1,28 @@
+package dao
+
+import model.Superhero
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.jdbc.JdbcProfile
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class SuperheroDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)
+                          (implicit executionContext: ExecutionContext) extends HasDatabaseConfigProvider[JdbcProfile] {
+  import profile.api._
+
+  private val Superheros = TableQuery[ProduktTable]
+
+  def all(): Future[Seq[Superhero]] = db.run(Superheros.result)
+
+  def insert(superhero: Superhero): Future[Unit] = db.run(Superheros += superhero).map { _ => () }
+
+  private class ProduktTable(tag: Tag) extends Table[Superhero](tag, "Superhero") {
+
+    def name = column[String]("NAME", O.PrimaryKey)
+    def element = column[String]("Element")
+
+
+    def * = (name, element) <> (Superhero.tupled, Superhero.unapply)
+  }
+}
