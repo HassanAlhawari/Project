8392e4051c8315799d0ce47380888a2d1412250c
Existime <hassan.alhawari98@gmail.com>
2021-05-29 13:45:43 +0200

    Superhero visual update 1.0

--- app/model/HeroBody.scala
+++ app/model/HeroBody.scala
@@ -1,6 +1,7 @@
 package model
 
-case class HeroBody ( name : String, healthmax : Int, healthcurrent : Int,  defense :  Int,
+case class HeroBody ( name : String, healthmax : Int , healthcurrent : Int,  defense :  Int,
                       attack : Int,  manamax : Int, manacurrent : Int,  statpoints : Int) {
+
 
 }
